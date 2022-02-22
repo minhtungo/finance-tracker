144fef375a92ea6fef6f338d54079b8c75f5e2af
minhtungo <loducfc@gmail.com>
2022-02-21 22:47:40 -0500

    finish authentication part

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -2,39 +2,29 @@ import { useState, useEffect } from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
-export const useSignup = () => {
+export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = async (email, password, displayName) => {
+  const logout = async () => {
     setError(null);
     setIsPending(true);
 
+    //sign the user out
     try {
-      // signup
-      const res = await projectAuth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-
-      if (!res) {
-        throw new Error('Could not complete signup');
-      }
-
-      // add display name to user
-      await res.user.updateProfile({ displayName });
-
-      // dispatch login action
-      dispatch({ type: 'LOGIN', payload: res.user });
-
+      await projectAuth.signOut();
+      //dispatch logout action
+      dispatch({ type: 'LOGOUT' });
+      //update state
       if (!isCancelled) {
         setIsPending(false);
         setError(null);
       }
     } catch (err) {
       if (!isCancelled) {
+        console.log(err.message);
         setError(err.message);
         setIsPending(false);
       }
@@ -44,6 +34,5 @@ export const useSignup = () => {
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
-
-  return { signup, error, isPending };
+  return { logout, error, isPending };
 };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import useSignup from '../../hooks/useSignup';
+import { useSignup } from '../../hooks/useSignup';
 
 // styles
 import styles from './Signup.module.css';
